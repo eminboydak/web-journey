2d818d9904741a8397d58895a95127cd24b4961a
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-07-21 02:00:56 +0300

    trying something

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+/* Default Imports */
+import logo from "./logo.svg";
+import "./App.css";
+
+/* My Imports */
+import Gallery from "./component/Galery";
+
+import "@discretize/gw2-ui-new/dist/default_style.css";
+import "@discretize/gw2-ui-new/dist/index.css";
+import "@discretize/typeface-menomonia";
+
+import { TraitLine } from "@discretize/gw2-ui-new";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TraitLine
+          id={41}
+          selected={[227, 214, 1672]}
+          onSelect={({ id, tier }) =>
+            console.log(`Clicked skill ${id} in tier ${tier}`)
+          }
+        />
       </header>
     </div>
   );
