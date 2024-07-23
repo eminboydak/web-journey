e3430f90e1e0115976a685914c5ffdae7fa4d458
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-07-23 23:30:12 +0300

    yapıyoz bi şeyler

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 /* Default Imports */
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 /* My Imports */
-import Gallery from "./component/Galery";
-
-import "@discretize/gw2-ui-new/dist/default_style.css";
-import "@discretize/gw2-ui-new/dist/index.css";
-import "@discretize/typeface-menomonia";
-
-import { TraitLine } from "@discretize/gw2-ui-new";
+import Header from './layouts/Header';
+import Landing from './layouts/Landing';
+import Body from './layouts/Body';
+import Footer from './layouts/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <TraitLine
-          id={41}
-          selected={[227, 214, 1672]}
-          onSelect={({ id, tier }) =>
-            console.log(`Clicked skill ${id} in tier ${tier}`)
-          }
-        />
+      <header className="App-header">  
+        <Header />
+        <Landing />
+        <Body />
+        <Footer />
       </header>
     </div>
   );
