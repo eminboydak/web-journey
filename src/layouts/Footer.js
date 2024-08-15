0c32f8a30f7a23089e712639f682c971fc453aa8
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-08-15 15:49:46 +0300

    road to nextjs

--- src/layouts/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="footer">
-      <p>Copyright © 2024 Muhammed Emin Boydak</p>
-    </div>
-  );
-}
--- src/layouts/Header.js
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <div className="topnav">
-      <a href="#">home</a>
-      <a href="#">about</a>
-      <a href="#">blog</a>
-    </div>
-  );
-}
