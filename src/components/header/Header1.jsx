6ade9e3915e1c580b51375f1ad239d3800a4cac6
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-08-15 18:20:44 +0300

    added example component and page

---
+++ src/components/header/Header1.jsx
@@ -0,0 +1,9 @@
+export default function Header1() {
+  return (
+    <>
+      <a href="#">home</a>
+      <a href="#">about</a>
+      <a href="#">blog</a>
+    </>
+  );
+}
---
+++ src/pages/header.js
@@ -0,0 +1,10 @@
+import Header1 from "@/components/header/Header1";
+
+export default function Header() {
+  return (
+    <>
+      <Header1 />
+      <br/>
+    </>
+  );
+}
