c682d78fcef0df4373ebdef7f7351cce9d984ef7
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-10-10 17:10:44 +0300

    page: Added example listing supabase page

--- src/pages/supabase.js
+++ src/pages/supabase.js
@@ -1,9 +1,31 @@
 import PageLayout from "@/components/layout/PageLayout";
+import { supabase } from "@/utils/supabaseClient";
 
-export default function Supabase() {
+export default function Supabase({ data }) {
   return (
     <PageLayout>
-      Deneme
+      <div>
+        <h1>Ürün Listesi</h1>
+        <ul>
+          {data.map((product) => (
+            <li key={product.id}>
+              <strong>{product.name}</strong> - {product.price} TL - Stok:{" "}
+              {product.stock}
+            </li>
+          ))}
+        </ul>
+      </div>
     </PageLayout>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const { data, error } = await supabase.from("products").select("*");
+
+  if (error) {
+    console.error("Data fetch error:", error);
+    return { props: { data: [] } };
+  }
+
+  return { props: { data } };
 }
