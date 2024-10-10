f535ead155aae83331430f4cdfa994821c825580
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-10-10 16:08:34 +0300

    layout: Edited menu items

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 
 const links = [
+  { name: "Home", url: "/"},
   { name: "Header", url: "/header" },
-  { name: "Header", url: "/header" },
+  { name: "Supabase", url: "/supabase" },
 ];
 
 export default function Header() {
