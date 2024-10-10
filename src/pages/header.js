98eaa54de15d879cea9a2f4ae273eae3484b5c13
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-10-10 16:08:59 +0300

    page: Edited header page

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,14 +1,15 @@
 import ArchHeader from "@/components/header/ArchHeader";
 import MintHeader, { Dropdown } from "@/components/header/MintHeader";
+import PageLayout from "@/components/layout/PageLayout";
 
 export default function Header() {
   return (
-    <>
+    <PageLayout>
       <ArchHeader />
-      <br/>
+      <br />
       <MintHeader />
-      <br/>
+      <br />
       <Dropdown />
-    </>
+    </PageLayout>
   );
 }
